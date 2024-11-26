a74b99b11aa7ed6afb6344b6420df51e1808a9f3
tringuyen@motive <95064327+tringuyenCoding@users.noreply.github.com>
2024-11-26 15:41:15 +0700

    Set up websocket

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import { Server } from "socket.io";
+import { createServer } from "http";
 
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
-
-app.use(express.static(path.join(__dirname, 'public')));  
-
-app.listen(PORT, () => { 
+const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-
 });
 
+const io = new Server(server);
+app.use(express.static(path.join(__dirname, "public")));
+
+let socketsConnected = new Set();
+
+io.on("connect", onConnected);
+
+function onConnected(socket) {
+  console.log(`Socket ${socket.id} connected`);
+  socketsConnected.add(socket.id);
+
+  io.emit("clients-total", socketsConnected.size);
+
+  socket.on("disconnect", () => {
+    console.log(`Socket ${socket.id} disconnected`);
+    socketsConnected.delete(socket.id);
+    io.emit("clients-total", socketsConnected.size);
+  });
+}
